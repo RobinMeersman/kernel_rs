962e34f25a10bc28e23d63ab42efb7db5f74b894
RobinMeersman <echteenrobin@gmail.com>
2024-06-28 10:36:23 +0200

    feat: post 1 finished

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Simple OS kernel in rust
 
 This is a toy project to learn about operating systems and rust without standard library etc.
+
+# Blog used
+This learning project follows the [Simple kernel in rust](https://os.phil-opp.com/freestanding-rust-binary/) blog
+from [phil-opp.com](https://phil-opp.com).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        // intentionally left empty for now
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {
+        // intentionally left empty for now
+    }
 }
